f3ecbfd9b4dedabfe977f13343d4644732b3e6d3
zhzj <amadeuszhang@outlook.com>
2022-05-12 23:14:00 +0200

    added .vscode

--- pairHMMs.c
+++ pairHMMs.c
@@ -5,36 +5,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+#include <string.h>
 #include <math.h>
+
+#define MAX_LEN 1000
 
 int main( ) {
     // INPUT :
     // R[], H[] : read bases and haplotype bases
-    // Q[] : quality scores (MANCANTE) (necessario per calcolo degli emission probabilities)
+    // Q[] : quality scores
     // T[][] : transition probabilities
     
     // parameters (given by GATK HaplotypeCaller)
-    double delta;           // indel start probability
+    float delta;           // indel start probability
     delta = 0.9;
-    double epsilon;         // indel continuation probability
+    float epsilon;         // indel continuation probability
     epsilon = 0.1;
 
     // define matrix of state transistion probabilities
-    double T[3][3] = {
+    float T[3][3] = {
         // poiché sono utilizzati separatamente, posso pensare di definire tre vettori anziché una matrice
-        { 1-2*delta, delta, delta },
+        { 1-(2*delta), delta, delta },
         { 1-epsilon, epsilon, 0 },
         { 1-epsilon, 0, epsilon }
     };
 
     // read in the sequence
-    char * seq1 = NULL;
-    char * seq2 = NULL;
+    char seq1[MAX_LEN] = "AGTGCTGAAAGTTGCGCCAGTGAC";
+    char seq2[MAX_LEN] = "AGTGCTGAAGTTCGCCAGTTGACG";
 
-    printf("Please input the first sequence: ");
-    scanf("%s", seq1);
-    printf("Please input the second sequence: ");
-    scanf("%s", seq2);
+    // printf("Please input the first sequence: ");
+    // scanf("%s", seq1);
+    // printf("Please input the second sequence: ");
+    // scanf("%s", seq2);
 
     int len1 = strlen(seq1);
     int len2 = strlen(seq2);
@@ -48,22 +53,25 @@ int main( ) {
     for ( int i = 0; i < len1; i++ ) {
         M[i][0] = I[i][0] = D[i][0] = 0;
     }
+    
     for ( int j = 0; j < len2; j++ ) {
         M[0][j] = I[0][j] = 0;
-        D[0][j] = 1/len2;
+        D[0][j] = FLT_MAX/len2;
     }
 
     // define the matrix of emission probabilities
     double lambda[len1][len2];
-    double Q[len1];
+    int q = 10;                         // constant default phred-scaled indel start quality
+    double Q = pow( 10, -q/10 );        // quality score
+    
     for ( int i = 0; i < len1; i++ ) {
         for ( int j = 0; j < len2; j++ ) {
             if ( seq1[i] == seq2[j] )
                 // match
-                lambda[i][j] = Q[i]/3;
+                lambda[i][j] = Q/3;
             else
                 // mismatch
-                lambda[i][j] = 1-Q[i];
+                lambda[i][j] = 1-Q;
             
             // fill in the rest of the matrix
             M[i][j] = lambda[i][j] * ( T[0][0] * M[i-1][j-1] + T[1][0] * I[i-1][j-1] + T[2][0] * D[i-1][j-1] );
@@ -75,6 +83,7 @@ int main( ) {
              * questo implica il fatto che gli elementi sulla stessa anti-diagonale non hanno dipendenza.
              * -> parallelizzare questo calcolo
              */
+
         }
     }
 
@@ -82,6 +91,9 @@ int main( ) {
     double finalScore = 0;
     for ( int j = 0; j < len2; j++ ) {
         finalScore += M[len1][j];
+        // printf("finalScore: %g\n", finalScore);
     }
+
+    printf("Final score: %f\n", finalScore);
      
 }
